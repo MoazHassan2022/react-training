59dffad70be5d127336631057e14a6463da213fd
MoazHassan2022 <moaz25jan2015@gmail.com>
2023-08-26 17:25:35 +0300

    initialized project structure

---
+++ portfolio-app/src/Components/Header.js
@@ -0,0 +1,54 @@
+import React from "react"
+import "./styles/Header.scss"
+import { Close, MenuBookOutlined } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__logo">
+                <h1>Moaz</h1>
+            </div>
+
+            <nav>
+                <ul>
+                    <div className="closed">
+                        <Close className='close' />
+                    </div>
+
+                    <li>
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            Blog
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="changer">
+                <MenuBookOutlined className="menu" />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
